20ee63f0ef5d2ef0853bf4fb453fdb646c75f8ab
Caden Mesina <113112958+yeahokpal@users.noreply.github.com>
2023-04-13 10:04:19 -0400

    Load Screen Works

--- Scripts/roster.js
+++ Scripts/roster.js
@@ -1,3 +1,16 @@
-$('rosterGrid').imagesLoaded( function() {
-    window.alert('Images Done');
+var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+
+[].forEach.call(imgs, function (img) {
+    if (img.complete) incrementCounter();
+    else img.addEventListener("load", incrementCounter, false);
 });
+
+function incrementCounter() {
+    counter++;
+    if (counter === len) {
+        console.log("All images loaded!");
+        $("#loadScreen").fadeOut("slow");
+    }
+}
